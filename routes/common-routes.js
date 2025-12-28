d09df02076b5a2c782a7eea9dfb73ce57219b2ae
GMANarasimha145 <madhuappalanarasimhag@gmail.com>
2025-12-28 18:54:43 +0530

    added test hello route

--- routes/common-routes.js
+++ routes/common-routes.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const {showProductsController} = require('../controllers/show-products-common-controller');
 
 router.get('/fetch-products', showProductsController);
+router.get('/test-hello', async(req, res)=>{
+    return res.status(200).json({
+        success : true,
+        message : "Hello this is madhu"
+    });
+});
 
 module.exports = router;
